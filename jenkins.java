5c7a5ab27bd5734f462818f9bd6fe485a54bea90
Illedo <102048092+Illedo@users.noreply.github.com>
2022-03-28 17:10:55 +0200

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -1,5 +1,5 @@
 public class jenkins{
     public static void main (String[] args){
-    System.out.print("Hola Jenkins soc el Ismael Lledó Rodellas");
+    System.out.print("Hola Jenkins soc el Ismael Lledo Rodellas");
     }
 }
